8b960c76f925df6256da6fdc8af9cd67965bbc27
Aranya Iyer <aranya6776@gmail.com>
2018-07-21 01:23:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function add(a,b){
-  var a = 8
-  var b = 10
-  return a + b
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+function add(a, b){
+    return a + b
 }
 add()
+
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+function subtract(a,b){
+    return a - b
+}
+subtract()
+
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+function multiply(a,b){
+    return a * b
+}
+multiply()
+
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+function divide(a,b){
+    return a / b
+}
+divide()
